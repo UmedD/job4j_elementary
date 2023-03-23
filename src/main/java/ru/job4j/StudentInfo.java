4cb05f55073e263f903557a32b7c210b08bd50f4
UmedD <umedjon721@gmail.com>
2023-03-23 23:53:51 +0500

    4.1. Разбор первой программы

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Umed");
+        System.out.println("27.08.1994");
+    }
+}
