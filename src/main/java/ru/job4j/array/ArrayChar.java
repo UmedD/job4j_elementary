a447558a90cfa8545fd1fbce2af15d4759d2bccb
UmedD <umedjon721@gmail.com>
2023-08-18 17:11:46 +0500

    6.4.0. Слово начинается с

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        int length = Math.min(word.length, pref.length);
+        int length = word.length > pref.length ? pref.length : word.length;
         for (int i = 0; i < length; i++) {
             if (word[i] != pref[i]) {
                 result = false;
